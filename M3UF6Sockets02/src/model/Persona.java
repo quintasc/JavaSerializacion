0fd51e26791d5105a23c1e497ab54c2b74bf1e56
quintasc <101179082+quintasc@users.noreply.github.com>
2024-04-23 14:34:10 +0200

    Add files via upload

---
+++ M3UF6Sockets02/src/model/Persona.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+
+public class Persona implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private int edad;   
+
+    public Persona() {
+    }
+
+    public Persona(String contenido, int edad) {
+        this.nombre = contenido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", edad=").append(edad);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+    
+}
